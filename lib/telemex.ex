b0d36c6d02612a3a56ee44a0c75eefd526a74e0c
Craig S. Cottingham <craig@cottingham.net>
2020-01-05 22:26:55 -0600

    Add Telemex.Application

--- lib/telemex.ex
+++ lib/telemex.ex
@@ -2,17 +2,4 @@ defmodule Telemex do
   @moduledoc """
   Documentation for Telemex.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Telemex.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
